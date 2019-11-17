2ca45bc8d4c1319cce39aee3d543c419a58600cf
Michael Eichenberger <mikeetpt@gmail.com>
2019-11-17 15:00:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sunset Notification
+Small Python application that sends a text message informing me when the sunset will be. (I like to turn on my external house lights on at sunset. I'm neurotic like that.)
---
+++ sunset_notify.py
@@ -0,0 +1,52 @@
+import os
+import sched
+import time as tm
+from datetime import date, datetime, time, timedelta
+
+import requests
+
+from bs4 import BeautifulSoup
+from twilio.rest import Client
+
+loc = '30.578806,-97.853065'
+url = f'https://darksky.net/forecast/{loc}/us12/en'
+
+sun_page = requests.get(url)
+sun = sun_page.content
+
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+
+scheduler = sched.scheduler(tm.time, tm.sleep)
+client = Client(account_sid, auth_token)
+
+soup = BeautifulSoup(sun, 'html.parser')
+
+sun_today = soup.find('span', {'class': 'sunset swap'})
+
+now = datetime.now()
+
+year = now.year
+month = now.month
+day = now.day
+hour = int(str(sun_today.contents[3])[-13:-12])
+mins = int(str(sun_today.contents[3])[-11:-9])
+
+sunset_time = time(hour=hour+12, minute=mins)
+
+delta = timedelta(minutes=5)
+
+msg_time = (datetime.combine(date(year, month, day), sunset_time) - delta).timestamp()
+
+str_time = sunset_time.strftime('%I:%M %p').lstrip('0')
+
+def send_sms(time):
+    message = client.messages.create(
+        to='+15129836197', from_='+18328955418', body=f'Turn on the lights at {time}'
+    )
+    return message
+
+
+scheduler.enterabs(msg_time, 1, send_sms, (str_time,))
+
+scheduler.run()
